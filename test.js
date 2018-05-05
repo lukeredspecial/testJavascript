81c1512f20461e4c1670368fba0336e4c917fc33
l.imola <l.imola@goproject.it>
2018-05-05 16:13:11 +0200

    add test file

---
+++ test.js
@@ -0,0 +1,44 @@
+"use strict";
+
+var cookieString = 'cookie1=prova;';
+var cookie2 ='cookie2=yet another test; expires=Sat, 5 May 2018 23:47:11 UTC; path=/';
+
+//console.log(cookieString);
+//document.onerror(alert("Error"));
+document.cookie = cookieString;
+
+function button1Click()
+{
+
+    alert(document.cookie);
+
+
+    var i = 0;
+    for(i=0;i<10;i++)
+    {
+        console.log("Il numero è " + i * 10);
+    }
+
+    var now = new Date();
+    
+    var expireTime = now.getTime() + 1000*36000;
+
+    now.setDate(expireTime);
+
+    var persona = {
+        nome:"Luca",
+        cognome:"Imola",
+        fullname: function(){return this.nome + " " + this.cognome},
+        json:function(){return '{"nome":"'+this.nome +'","cognome":"'+ this.cognome +'"}'}
+    }
+
+    alert(persona.json());
+    var parsed = JSON.parse(persona.json());
+
+    alert(parsed.nome);
+
+    //console.log(cookieString);
+    //console.log("test 3");
+
+}
+
